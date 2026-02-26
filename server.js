a67cbd1d43af135bb867f24d3e2d8a9f3e2c2698
Koushikpilli <koushikrc2005@gmail.com>
2026-02-26 23:24:12 +0530

    Refactor server.js to use path for static files

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 connectDB();
 
@@ -18,4 +20,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
 });
